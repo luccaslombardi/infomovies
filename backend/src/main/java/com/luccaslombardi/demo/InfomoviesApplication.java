f7df55765743d531ba2b20141a88975f37622534
Luccas Lombardi <luccas.lombardi@hotmail.com>
2022-03-08 14:24:29 -0300

    Criação do Projeto

---
+++ backend/src/main/java/com/luccaslombardi/demo/InfomoviesApplication.java
@@ -0,0 +1,13 @@
+package com.luccaslombardi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfomoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfomoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luccaslombardi/demo/InfomoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luccaslombardi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfomoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
