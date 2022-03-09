0235743694da652132f9e995be061ee88da8c1a1
Luccas Lombardi <luccas.lombardi@hotmail.com>
2022-03-09 20:19:49 -0300

    Domain model, database seed

--- backend/src/main/java/com/luccaslombardi/infomovies/InfomoviesApplication.java
+++ backend/src/main/java/com/luccaslombardi/infomovies/InfomoviesApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InfomoviesApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(InfomoviesApplication.class, args);
 	}
 
